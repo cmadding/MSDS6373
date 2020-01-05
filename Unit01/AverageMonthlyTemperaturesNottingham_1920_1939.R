2e52e6024f0a352f3b136b6e0bb31bd769f730a9
Chad Madding <cmadding@smu.edu>
2020-01-05 17:07:39 -0600

    Unit 1 homework update

---
+++ Unit01/AverageMonthlyTemperaturesNottingham_1920_1939.R
@@ -0,0 +1,68 @@
+#Description
+#A regular time-series object containing average air temperatures at Nottingham Castle in degrees F for 20 years.
+
+#Source
+#Anderson, O.D. (1976) Time Series Analysis and Forecasting: The Box-Jenkins approach. Butterworths. Series R.
+
+#Usage
+#nottem
+#install.packages("tseries")
+
+require(stats)
+require(graphics)
+library(xtable)
+library(tseries)
+
+
+#Average Monthly Temperatures at Nottingham, 1920–1939
+data(nottem)
+ts(nottem, start=c(1920, 1), end=c(1939, 12), frequency=12)
+start(nottem)
+end(nottem)
+length(nottem)
+dim(nottem)
+names(nottem)
+summary(nottem)
+
+plot(nottem,type = "b",main = "Average Monthly Temperatures at Nottingham, 1920–1939", ylab="Air Temperature (Degrees Fahrenheit)", xlab="Month/Year")
+abline(reg=lm(nottem~time(nottem)))
+
+nott <- window(nottem, end = c(1936,12))
+
+fit <- arima(nott, order = c(1,0,0), list(order = c(2,1,0), period = 12))
+
+nott.fore <- predict(fit, n.ahead = 36)
+
+ts.plot(nott, nott.fore$pred, nott.fore$pred+2*nott.fore$se,
+        nott.fore$pred-2*nott.fore$se, gpars = list(col = c(1,1,4,4)))
+
+# Beginning Time Series Analysis
+nottemtimeseries = ts(nottem, frequency=12, start=c(1920,1),end=c(1939, 12))
+nt = xtable(nottemtimeseries)
+nt
+
+#Plot of Timeserires
+plot(nottemtimeseries, col = "blue",lwd=2,lty=1, main="TIME SERIES PLOT OF NOTTEM",cex.main=1)
+plot.ts(nottem, col = "blue",lwd=2,lty=1, main="TIME SERIES PLOT OF NOTTEM",cex.main=1)
+
+#ACF
+acf(nottem[1:240], main="ACF of Nottingham")
+acf(nottem[1:120],plot=TRUE, main="ACF of Nottingham 1st Half")
+acf(nottem[120:240],plot=TRUE, main="ACF of Nottingham 2nd Half")
+
+acf(nottem, lag.max=12) # plot a correlogram
+
+## Transform the lags from years to months
+nottempl = acf(nottem,lag.max=12, plot=FALSE)
+nottempl$lag <- nottempl$lag * 12
+## Plot the acf with months 
+plot(nottempl, main="ACF of Nottingham", xlab="Lag (months)")
+
+#PACF
+pacf(nottem, main="PACF of Nottingham")
+pacf(nottemtimeseries, lag.max=20) # plot a correlogram
+pacf(nottemtimeseries, lag.max=20, plot=FALSE)
+
+#Augmented Dickey-Fuller Test
+adf.test(nottem)
+
--- Unit01/SampleDataSets.R
+++ Unit01/SampleDataSets.R
@@ -2,6 +2,26 @@
 
 library(tswge)
 
+
+#West Texas Intermediate Crude Oil Prices
+data(wtcrude)
+ts(wtcrude, start=c(2000, 1), end=c(2009, 10), frequency=12)
+class(wtcrude)
+start(wtcrude)
+end(wtcrude)
+plot(wtcrude,type = "l",main = "West Texas Intermediate Crude Oil Prices ( January 2000 through October 2009)", ylab="Price", xlab="Month")
+cycle(wtcrude)
+abline(reg=lm(wtcrude~time(wtcrude)))
+plot(aggregate(wtcrude,FUN=mean))
+
+#Pennsylvania average monthly temperatures
+data(patemp)
+ts(patemp, start=c(1990, 1), end=c(2004, 12), frequency=12)
+start(patemp)
+end(patemp)
+plot(patemp,type = "b",main = "Pennsylvania Average Monthly Temperatures (January 1990 to December 2004)", ylab="Temperature (Degrees Fahrenheit)", xlab="Month")
+abline(reg=lm(patemp~time(patemp)))
+
 data("airline")
 plot(airline,type = "l")
 data("cement")
@@ -9,3 +29,6 @@ plot(cement,type = "l")
 data("chirp")
 plot(chirp,type = "l")
 
+#Whale click data
+data("whale")
+plot(whale,type = "l")
