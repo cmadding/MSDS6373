1955bb26fd8d8cf71ae520b8db4c226d9ec4df78
Chad Madding <cmadding@smu.edu>
2020-01-18 14:59:51 -0600

    Finish up Unit 3 live work

--- Unit03/Walmart Spectral Density R Code.R
+++ Unit03/Walmart Spectral Density R Code.R
@@ -1,13 +1,15 @@
 #install.packages("tidyverse")
 library(tidyverse) # needs to be run every time you start R and want to use %>%
 library(tswge)
+library(tseries)
+library(stats)
 
 #Walmart Store 8Item 1 Filtering / spectral analysis / AR(3)
 # Read in the data
 Walmart = read.csv("Data/Walmart.csv",header = TRUE)
 
 # Load the Data
-Stor8Item1 = Walmart %>% filter(item == 1 & store == 8)
+Stor8Item1 = Walmart %>% dplyr::filter(item == 1 & store == 8)
 
 #Look at and Visualize the data
 head(Stor8Item1)
@@ -51,6 +53,24 @@ acf(Stor8Item1_grouped$mean_sales[1:30],plot=TRUE, main="ACF of Wal Mart 1st Hal
 acf(Stor8Item1_grouped$mean_sales[31:60],plot=TRUE, main="ACF of Wal Mart 2nd Half")
 
 plotts.sample.wge(Stor8Item1_grouped$mean_sales)
+
+#Setup a 5 Point Moving Average Filter
+ma5 = stats::filter(Stor8Item1_grouped$mean_sales,rep(1,5))/5
+ma5 = na.remove(ma5)
+
+#Plot the Low Pass 5 Point Moving Average
+plot(ma5,type = 'l')
+parzen.wge(ma5)
+plotts.sample.wge(ma5)
+
+#Setup a 51 Point Moving Average Filter
+ma51 = stats::filter(Stor8Item1_grouped$mean_sales,rep(1,51))/51
+ma51 = na.remove(ma51)
+
+#Plot the Low Pass 51 Point Moving Average
+plot(ma51,type = 'l')
+parzen.wge(ma51)
+plotts.sample.wge(ma51)
 
 #Create a test set
 Stor8Item1_season = ts(Stor8Item1_grouped$mean_sales,  frequency = 12)
